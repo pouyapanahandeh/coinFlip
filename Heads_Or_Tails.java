1dcbf47dc496c65918dbaf178895763a463c1b57
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-04-25 21:26:58 +0200

    method added

--- Heads_Or_Tails.java
+++ Heads_Or_Tails.java
@@ -2,7 +2,14 @@ import java.util.Scanner;
 
 
 public class Heads_Or_Tails{
+
 		//--------  Exercise-1 -------------
 		// Compute and display the number of times did it occur in the trial that exactly two heads
 		// were tossed after each other
+		private static void exercise1(int N, String trial) {
+
+
+
+
+		}
 }
