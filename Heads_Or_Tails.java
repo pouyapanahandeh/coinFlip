603d707ce1200e067c831c9663e6faf04016c6d5
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-04-25 21:26:02 +0200

    First ex

--- Heads_Or_Tails.java
+++ Heads_Or_Tails.java
@@ -2,5 +2,7 @@ import java.util.Scanner;
 
 
 public class Heads_Or_Tails{
-
+		//--------  Exercise-1 -------------
+		// Compute and display the number of times did it occur in the trial that exactly two heads
+		// were tossed after each other
 }
